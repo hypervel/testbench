459dae6d6d646a98ad044e67bb07ce686ff31811
Yu Shing <yushing@universetech.cc>
2025-09-22 11:38:01 +0800

    feat: migrate test of Horizon

--- workbench/config/database.php
+++ workbench/config/database.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Hypervel\Support\Str;
+
 return [
     'default' => env('DB_CONNECTION', 'sqlite'),
     'connections' => [
@@ -10,6 +12,26 @@ return [
             'database' => ':memory:',
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+    ],
+    'redis' => [
+        'options' => [
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'hypervel'), '_') . '_database_'),
+        ],
+
+        'default' => [
+            'host' => env('REDIS_HOST', 'localhost'),
+            'auth' => env('REDIS_AUTH', null),
+            'port' => (int) env('REDIS_PORT', 6379),
+            'db' => (int) env('REDIS_DB', 0),
+            'pool' => [
+                'min_connections' => 1,
+                'max_connections' => 10,
+                'connect_timeout' => 10.0,
+                'wait_timeout' => 3.0,
+                'heartbeat' => -1,
+                'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 60),
+            ],
         ],
     ],
 ];
