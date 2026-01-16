aef4774be8ce862a837c75dfe9b8464c44c8f79f
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2026-01-16 15:21:17 +0000

    style: apply php-cs-fixer formatting to testbench routes

--- src/Concerns/HandlesRoutes.php
+++ src/Concerns/HandlesRoutes.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hypervel\Testbench\Concerns;
 
 use Hypervel\Router\Router;
+use ReflectionMethod;
 
 /**
  * Provides hooks for defining test routes.
@@ -44,7 +45,7 @@ trait HandlesRoutes
 
         // Only set up web routes group if the method is overridden
         // This prevents empty group registration from interfering with other routes
-        $refMethod = new \ReflectionMethod($this, 'defineWebRoutes');
+        $refMethod = new ReflectionMethod($this, 'defineWebRoutes');
         if ($refMethod->getDeclaringClass()->getName() !== self::class) {
             $router->group('/', fn () => $this->defineWebRoutes($router), ['middleware' => ['web']]);
         }
